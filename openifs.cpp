531dc219da7caa66f2066ee0736f3fa9f61bc6ef
abowery <abowery@users.noreply.github.com>
2018-12-01 17:13:00 +0000

    Initial check-in of openifs.cpp
    This is the first version of the controller program for OpenIFS in climateprediction.net.

---
+++ openifs.cpp
@@ -0,0 +1,353 @@
+//
+// Control code for the OpenIFS application in the climateprediction.net project
+//
+// Written by Andy Bowery (Oxford eResearch Centre, Oxford University) November 2018
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include "./boinc/api/boinc_api.h"
+#include "./boinc/zip/boinc_zip.h"
+
+int main(int argc, char** argv) {
+    using namespace std::chrono;
+    using namespace std::this_thread;
+    using namespace std;
+    int retval = 0;
+
+    boinc_init();
+    boinc_parse_init_data_file();
+
+    // Get BOINC user preferences
+    APP_INIT_DATA dataBOINC;
+    boinc_get_init_data(dataBOINC);
+
+    // Set BOINC optional values
+    BOINC_OPTIONS options;
+    boinc_options_defaults(options);
+    options.main_program = true;
+    options.multi_process = true;
+    options.check_heartbeat = true;
+    options.handle_process_control = true;  // monitor handles all suspend/quit/resume
+    options.direct_process_action = false;  // monitor won't get suspended/killed by boinc
+    options.send_status_msgs = false;
+
+    retval = boinc_init_options(&options);
+    if (retval) {
+       fprintf(stderr, "BOINC init options failed!\n");
+    }
+
+    // Get the project path
+    std::string project_path = dataBOINC.project_dir + std::string("/");
+    fprintf(stderr,"current project directory is: %s\n", project_path.c_str());
+
+    // Use the below line for setting the project path when running in standalone
+    //std::string project_path = "/home/abowery/Desktop/OpenIFS/projects/";
+
+    // Get the slots path (the current working path)
+    char slot_path[256];
+    if (getcwd(slot_path, sizeof(slot_path)) == NULL)
+      fprintf(stderr,"getcwd() returned an error\n");
+    else
+      fprintf(stderr,"current working directory is: %s\n", slot_path);
+
+    fprintf(stderr, "(argv0) %s\n", argv[0]);
+    fprintf(stderr, "(argv1) exptid: %s\n", argv[1]);
+    fprintf(stderr, "(argv2) batchid: %s\n", argv[2]);
+    fprintf(stderr, "(argv3) version: %s\n", argv[3]);
+    fprintf(stderr, "(argv4) wuid: %s\n", argv[4]);
+    fflush(stderr);
+
+    // Read the exptid, batchid, version, wuid from the command line
+    std::string exptid  = argv[1];
+    std::string batchid = argv[2];
+    std::string version = argv[3];
+    std::string wuid    = argv[4];
+
+    boinc_begin_critical_section();
+
+    // Copy apps files to working directory
+    std::string var1 = std::string("cp ") + project_path + \
+                       std::string("openifs_app_") + version + std::string(".zip ") + slot_path;
+    fprintf(stderr, "Copying apps to working directory: %s\n", var1.c_str());
+    fflush(stderr);
+    system(var1.c_str());
+
+    // Copy namelist files to working directory
+    std::string var2 = std::string("cp ") + project_path + \
+                       std::string("openifs_wu_") + exptid + std::string(".zip ") + slot_path;
+    fprintf(stderr, "Copying namelist files to working directory: %s\n", var2.c_str());
+    fflush(stderr);
+    system(var2.c_str());
+
+    // Copy ancils files to working directory
+    std::string var3 = std::string("cp ") + project_path + \
+                       std::string("openifs_ic_ancil_") + exptid + std::string(".zip ") + slot_path;
+    fprintf(stderr, "Copying ancils files to working directory: %s\n", var3.c_str());
+    fflush(stderr);
+    system(var3.c_str());
+
+    // Copy ifsdata files to working directory
+    std::string var4 = std::string("cp ") + project_path + \
+                       std::string("ifsdata_") + exptid + std::string(".zip ") + slot_path;
+    fprintf(stderr, "Copying ifsdata files to working directory: %s\n", var4.c_str());
+    fflush(stderr);
+    system(var4.c_str());
+
+    sleep_until(system_clock::now() + seconds(60));
+
+    // Unzip the app zip file
+    std::string var9 = slot_path + std::string("/openifs_app_") + version + std::string(".zip");
+    fprintf(stderr, "Unzipping app zip file: %s\n", var9.c_str());
+    fflush(stderr);
+    boinc_zip(UNZIP_IT,var9.c_str(),slot_path);
+
+    // Unzip the ancils zip file
+    std::string var10 = slot_path + std::string("/openifs_wu_") + exptid + std::string(".zip");
+    fprintf(stderr, "Unzipping namelist zip file: %s\n", var10.c_str());
+    fflush(stderr);
+    boinc_zip(UNZIP_IT,var10.c_str(),slot_path);
+
+    // Unzip the ancils zip file
+    std::string var11 = slot_path + std::string("/openifs_ic_ancil_") + exptid + std::string(".zip");
+    fprintf(stderr, "Unzipping ancils zip file: %s\n", var11.c_str());
+    fflush(stderr);
+    boinc_zip(UNZIP_IT,var11.c_str(),slot_path);
+
+    // Unzip the ifsdata zip file
+    std::string var12 = slot_path + std::string("/ifsdata_") + exptid + std::string(".zip");
+    fprintf(stderr, "Unzipping ifsdata files: %s\n", var12.c_str());
+    fflush(stderr);
+    boinc_zip(UNZIP_IT,var12.c_str(),slot_path);
+
+    // Make the ifsdata directory
+    std::string var13a = slot_path + std::string("/openifs_app/ifsdata");
+    if (mkdir(var13a.c_str(),S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH) != 0) fprintf(stderr, "mkdir() ifsdata failed\n");
+
+    // Move ifsdata files to app directory
+    std::string var13 = std::string("mv ") + slot_path + std::string("/ifsdata_") + exptid + \
+                        std::string("/* ") + slot_path + std::string("/openifs_app/ifsdata");
+    fprintf(stderr, "Moving ifsdata files to app directory: %s\n", var13.c_str());
+    fflush(stderr);
+    system(var13.c_str());
+
+    // Move workunit files to app directory
+    std::string var14 = std::string("mv ") + slot_path + std::string("/openifs_wu_") + exptid + \
+                        std::string("/* ") + slot_path + std::string("/openifs_app");
+    fprintf(stderr, "Move workunit files to app directory: %s\n", var14.c_str());
+    fflush(stderr);
+    system(var14.c_str());
+
+    // Move ancil files to app directory
+    std::string var15 = std::string("mv ") + slot_path + std::string("/openifs_ic_ancil_") + exptid + \
+                        std::string("/* ") + slot_path + std::string("/openifs_app");
+    fprintf(stderr, "Move ancil files to app directory: %s\n",var15.c_str());
+    fflush(stderr);
+    system(var15.c_str());
+
+    char *pathvar;
+    // Set the GRIB_SAMPLES_PATH environmental variable
+    std::string var16 = std::string("GRIB_SAMPLES_PATH=") + slot_path + \
+                        std::string("/openifs_app/eccodes/ifs_samples/grib1_mlgrib2");
+    if (putenv((char *)var16.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("GRIB_SAMPLES_PATH");
+    fprintf(stderr, "The current GRIB_SAMPLES_PATH is: %s\n", pathvar);
+
+    // Set the GRIB_DEFINITION_PATH environmental variable
+    std::string var17 = std::string("GRIB_DEFINITION_PATH=") + slot_path + \
+                        std::string("/openifs_app/eccodes/definitions");
+    if (putenv((char *)var17.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("GRIB_DEFINITION_PATH");
+    fprintf(stderr, "The current GRIB_DEFINITION_PATH is: %s\n", pathvar);
+
+    // Change permissions on the app directory
+    std::string var18 = std::string("chmod -R 777 ") + slot_path + std::string("/openifs_app");
+    fprintf(stderr, "Changing permissions: %s\n", var18.c_str());
+    fflush(stderr);
+    system(var18.c_str());
+
+    // Change directory to the app directory
+    std::string var18a = slot_path + std::string("/openifs_app/");
+    if (chdir(var18a.c_str()) != 0) fprintf(stderr, "chdir() failed to: %s\n",var18a.c_str());
+
+    // Defaults to input arguments
+    int OIFS_RUN=1;                   // run number, output will be saved to directory: output$OIFS_RUN
+    int OIFS_RES=159;	              // model resolution
+    std::string OIFS_EXPID = exptid;  // model experiment id, must match string in filenames
+    std::string OIFS_EXE;             // OpenIFS executable - not used
+    int NPROC=8;                      // number of MPI tasks, need to be set in fort.4 as well
+    int NTHREADS=1;                   // default number of OPENMP threads
+    std::string NAMELIST="fort.4";    // NAMELIST file, usually taken from 'ecmwf' subdirectory
+    int TIMESTEP=3600;
+    std::string FCLEN = "d1";
+    std::string FCLEN_TIMESTEP=" -f "+FCLEN+" -t "+std::to_string(TIMESTEP);
+
+    // Set the OIFS_DUMMY_ACTION environmental variable, this controls what OpenIFS does if it goes into a dummy subroutine
+    // Possible values are: 'quiet', 'verbose' or 'abort'
+    std::string var19a = std::string("OIFS_DUMMY_ACTION=abort");
+    if (putenv((char *)var19a.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("OIFS_DUMMY_ACTION");
+    fprintf(stderr, "The current OIFS_DUMMY_ACTION is: %s\n", pathvar);
+
+    // Set the OMP_NUM_THREADS environmental variable, the number of threads
+    std::string var19b = std::string("OMP_NUM_THREADS=") + std::to_string(NTHREADS);
+    if (putenv((char *)var19b.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("OMP_NUM_THREADS");
+    fprintf(stderr, "The current OMP_NUM_THREADS is: %s\n", pathvar);
+
+    // Set the OMP_SCHEDULE environmental variable, this enforces static thread scheduling
+    std::string var19c = std::string("OMP_SCHEDULE=STATIC");
+    if (putenv((char *)var19c.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("OMP_SCHEDULE");
+    fprintf(stderr, "The current OMP_SCHEDULE is: %s\n", pathvar);
+
+    // Set the DR_HOOK environmental variable, this controls the tracing facility in OpenIFS, off=0 and on=1
+    std::string var19ca = std::string("DR_HOOK=1");
+    if (putenv((char *)var19ca.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("DR_HOOK");
+    fprintf(stderr, "The current DR_HOOK is: %s\n", pathvar);
+
+    // Set the DR_HOOK_HEAPCHECK environmental variable, this ensures the heap size statistics are reported
+    std::string var19cb = std::string("DR_HOOK_HEAPCHECK=no");
+    if (putenv((char *)var19cb.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("DR_HOOK_HEAPCHECK");
+    fprintf(stderr, "The current DR_HOOK_HEAPCHECK is: %s\n", pathvar);
+
+    // Set the DR_HOOK_STACKCHECK environmental variable, this ensures the stack size statistics are reported
+    std::string var19cc = std::string("DR_HOOK_STACKCHECK=no");
+    if (putenv((char *)var19cc.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("DR_HOOK_STACKCHECK");
+    fprintf(stderr, "The current DR_HOOK_STACKCHECK is: %s\n", pathvar);
+
+    // Set the OMP_STACKSIZE environmental variable, OpenIFS needs more stack memory per process
+    std::string var19d = std::string("OMP_STACKSIZE=128M");
+    if (putenv((char *)var19d.c_str())) {
+      fprintf(stderr, "putenv failed \n");
+      return 1;
+    }
+    pathvar = getenv("OMP_STACKSIZE");
+    fprintf(stderr, "The current OMP_STACKSIZE is: %s\n", pathvar);
+
+    // Check for existence of NAMELIST
+    std::string namelist_file = slot_path + std::string("/openifs_app/") + NAMELIST;
+    struct stat buffer;
+    if(NAMELIST != "fort.4") {
+       fprintf(stderr, "NAMELIST file path is: %s\n",namelist_file.c_str());
+       fflush(stderr);
+       if (stat((char *)namelist_file.c_str(), &buffer) < 0){
+          fprintf(stderr, "NAMELIST file does not exist: %s\n",NAMELIST.c_str());
+       }
+       // Rename the NAMELIST file to fort.4
+       std::string var19e = std::string("mv ") + namelist_file + slot_path + std::string("/openifs_app/fort.4");
+       fprintf(stderr, "Renaming NAMELIST file: %s\n", var19e.c_str());
+       fflush(stderr);
+       system(var19e.c_str());
+    }
+    if (stat((char *)namelist_file.c_str(), &buffer) < 0) {
+       fprintf(stderr, "NAMELIST file does not exist: %s\n",NAMELIST.c_str());
+    }
+
+    // Set the core dump size to 0
+    struct rlimit core_limits;
+    core_limits.rlim_cur = core_limits.rlim_max = 0;
+    if (setrlimit(RLIMIT_CORE, &core_limits) != 0) fprintf(stderr, "setting core dump size to 0 failed\n");
+
+    // Set the stack limit to be unlimited
+    struct rlimit stack_limits;
+    stack_limits.rlim_cur = stack_limits.rlim_max = RLIM_INFINITY;
+    if (setrlimit(RLIMIT_STACK, &stack_limits) != 0) fprintf(stderr, "setting stack limit to unlimited failed\n");
+
+    // Start the OpenIFS job
+    std::string var19 = std::string("cd ") + slot_path + \
+                        std::string("/openifs_app;./master.exe -e ") + exptid + FCLEN_TIMESTEP;
+    fprintf(stderr, "Starting the executable: %s\n", var19.c_str());
+    fflush(stderr);
+    system(var19.c_str());
+
+    sleep_until(system_clock::now() + seconds(5));
+
+    // Make the results folder
+    std::string var20a = slot_path + std::string("/openifs_result_") + wuid;
+    if (mkdir(var20a.c_str(),S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH) != 0) fprintf(stderr, "mkdir() openifs_result failed\n");
+
+    // Move the results to the results folder
+    std::string var20 = std::string("mv -t ") + slot_path + std::string("/openifs_result_") + wuid + std::string(" ") + \
+                        slot_path + std::string("/openifs_app/ICM*+* ") + slot_path + \
+                        std::string("/openifs_app/NODE* ") + slot_path + \
+                        std::string("/openifs_app/ifs.stat");
+    fprintf(stderr, "Moving the results to the results folder: %s\n", var20.c_str());
+    fflush(stderr);
+    system(var20.c_str());
+
+    boinc_end_critical_section();
+
+    // Change directory to the slot path
+    if (chdir(slot_path) != 0) fprintf(stderr, "chdir() failed to: %s\n",slot_path);
+
+    // Tar the results files
+    std::string var22 = std::string("tar cf openifs_result_") + wuid + std::string(".tar openifs_result_") + wuid + \
+                        std::string("/ICM*+* openifs_result_") + wuid + std::string("/NODE* openifs_result_") + wuid + \
+                        std::string("/ifs.stat");
+    fprintf(stderr, "Tarring: %s\n", var22.c_str());
+    fflush(stderr);
+    system(var22.c_str());
+
+    sleep_until(system_clock::now() + seconds(20));
+
+    // Copy results files to script directory
+    std::string var23 = std::string("cp ") + slot_path + \
+                        std::string("/openifs_result_") + wuid + std::string(".tar ") + project_path;
+    fprintf(stderr, "Copying results files to script directory: %s\n", var23.c_str());
+    fflush(stderr);
+    system(var23.c_str());
+
+    sleep_until(system_clock::now() + seconds(20));
+
+    fprintf(stderr, "Checkpoint 1\n");
+    fflush(stderr);
+
+    // Upload the results file
+    std::string var24 = project_path + std::string("/openifs_result_") + wuid + std::string(".tar");
+    fprintf(stderr, "File being uploaded: %s\n", var24.c_str());
+    fflush(stderr);
+    boinc_upload_file(var24);
+
+    fprintf(stderr, "Checkpoint 2\n");
+    fflush(stderr);
+
+    boinc_finish(0);
+    return 0;
+}
